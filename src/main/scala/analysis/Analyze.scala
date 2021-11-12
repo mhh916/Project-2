7a97261fa7b3210b651af67eb92cfa9e932afecb
natshenk <77948563+natshenk@users.noreply.github.com>
2021-11-12 14:19:20 -0500

    Finished queries for analysis question 7

--- src/main/scala/analysis/Analyze.scala
+++ src/main/scala/analysis/Analyze.scala
@@ -1,6 +1,7 @@
 package analysis
 
 import setup.SparkConnect
+import org.apache.spark.sql._
 
 class Analyze   (){
     val sc = new SparkConnect()
@@ -30,7 +31,19 @@ class Analyze   (){
     }
 
     def q7(): Unit = {
-        
+        sc.getDataFrame.createOrReplaceTempView("liquor")
+
+        // Show the top 5 most profitbale liquors by their average profit (bottle retail cost - bottle state cost)
+        println("5 most profitable liquors (retail cost - state cost):")
+        val highestAvgDF = sc.getSparkSession.sql("SELECT `Item Description`, CAST(AVG(`State Bottle Retail` - `State Bottle Cost`) AS DECIMAL(10,2))" +
+                                    " AS `Average Profit (Dollars)` FROM liquor GROUP BY `Item Description` ORDER BY `Average Profit (DOLLARS)` DESC LIMIT 5")
+        highestAvgDF.show(false)
+
+        // Show the top 5 least profitbale liquors by their average profit (bottle retail cost - bottle state cost)
+        println("5 least profitable liquors (retail cost - state cost):")
+        val lowestAvgDF = sc.getSparkSession.sql("SELECT `Item Description`, CAST(AVG(`State Bottle Retail` - `State Bottle Cost`) AS DECIMAL(10,2))" +
+                                    " AS `Average Profit (Dollars)` FROM liquor GROUP BY `Item Description` ORDER BY `Average Profit (Dollars)` ASC LIMIT 5")
+        lowestAvgDF.show(false)
     }
 
     def q8(): Unit = {
