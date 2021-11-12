c3a6af3ea233cc7e2b652bd1454645baed8698fd
theehartin <91913346+theehartin@users.noreply.github.com>
2021-11-12 13:27:17 -0800

    Second half of Question 2

--- src/main/scala/analysis/groupByCounty.scala
+++ src/main/scala/analysis/groupByCounty.scala
@@ -25,7 +25,10 @@ class CountyAgg  (){
     println("Top 5 Counties That Spent the Most Money on Liquor: ")
     sc.spark.sql("SELECT DISTINCT UPPER(County) AS COUNTY, CAST(ROUND(SUM(CAST(`Sale (Dollars)` AS Double)) Over(PARTITION BY UPPER(County))/1000000,2) AS DECIMAL(10,2)) AS `Total Sales(mil)` FROM Liquor ORDER BY `Total Sales(mil)` DESC LIMIT 5").show()
 
-
-   
+    //THIS WORKS
+    println("Top 5 Counties That Purchased the Largest Quantity of Liquor: ")
+    println("NOTE: A barrel is 53 Gallons")
+    sc.spark.sql("SELECT DISTINCT UPPER(County) AS COUNTY, CAST(ROUND(SUM(CAST(`Volume Sold (Gallons)` AS Double)) Over(PARTITION BY UPPER(County))/53,2) AS DECIMAL(10,2)) AS `Total Volume(Barrels)` FROM Liquor ORDER BY `Total Volume(Barrels)` DESC LIMIT 5").show()
+    println()
   }
 }
