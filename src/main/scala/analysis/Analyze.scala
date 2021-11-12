0277c549a7210dee10e6f29ba3a78b476b6ced0c
Michael Hayes <michaelhayes22222@gmail.com>
2021-11-12 16:18:01 -0500

    Created initial query for first half of question.

--- src/main/scala/analysis/Analyze.scala
+++ src/main/scala/analysis/Analyze.scala
@@ -1,6 +1,7 @@
 package analysis
 
 import setup.SparkConnect
+import org.apache.spark.sql._
 
 class Analyze   (){
     val sc = new SparkConnect()
@@ -22,7 +23,16 @@ class Analyze   (){
     }
 
     def q5(): Unit = {
+        sc.getDataFrame.createOrReplaceTempView("liquor")
         
+        //Which vendor had the most revenue liquor sales? What was their best-selling category?
+        println("Getting vendor with most Revenue, Limit 3")
+        val q5 = sc.getSparkSession().sql("SELECT DISTINCT UPPER(`Vendor Name`) AS Vendor, ROUND(SUM(CAST(`State Bottle Cost` AS Double)) OVER(PARTITION BY UPPER(`Vendor Name`))/1000000,2) AS `Revenue(mil)`  FROM liquor ORDER BY `Revenue(mil)` DESC LIMIT 3")
+        q5.show(false)
+        // Grabs the 3 vendor names from dataframe
+        val vendor1 = q5.collect()(0)(0)
+        val vendor2 = q5.collect()(1)(0)
+        val vendor3 = q5.collect()(2)(0)
     }
 
     def q6(): Unit = {
