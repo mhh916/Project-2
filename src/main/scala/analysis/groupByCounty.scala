a09b9800f2edca068874b29f31a032586556e0de
theehartin <91913346+theehartin@users.noreply.github.com>
2021-11-12 16:04:41 -0800

    Added 'Per City'

--- src/main/scala/analysis/groupByCounty.scala
+++ src/main/scala/analysis/groupByCounty.scala
@@ -30,5 +30,17 @@ class CountyAgg  (){
     println("NOTE: A barrel is 53 Gallons")
     sc.spark.sql("SELECT DISTINCT UPPER(County) AS COUNTY, CAST(ROUND(SUM(CAST(`Volume Sold (Gallons)` AS Double)) Over(PARTITION BY UPPER(County))/53,2) AS DECIMAL(10,2)) AS `Total Volume(Barrels)` FROM Liquor ORDER BY `Total Volume(Barrels)` DESC LIMIT 5").show()
     println()
+
+    //Sales per City
+    println("Top 5 Cities That Spent the Most Money on Liquor: ")
+    sc.spark.sql("SELECT DISTINCT UPPER(City) AS City, CAST(ROUND(SUM(CAST(`Sale (Dollars)` AS Double)) Over(PARTITION BY UPPER(City))/1000000,2) AS DECIMAL(10,2)) AS `Total Sales(mil)` FROM Liquor ORDER BY `Total Sales(mil)` DESC LIMIT 5").show()
+
+
+    //Sales per City
+    println("Top 5 Cities That Purchased the Largest Quantity of Liquor: ")
+    println("NOTE: A barrel is 53 Gallons")
+    sc.spark.sql("SELECT DISTINCT UPPER(City) AS City, CAST(ROUND(SUM(CAST(`Volume Sold (Gallons)` AS Double)) Over(PARTITION BY UPPER(City))/53,2) AS DECIMAL(10,2)) AS `Total Volume(Barrels)` FROM Liquor ORDER BY `Total Volume(Barrels)` DESC LIMIT 5").show()
+    println()
+
   }
 }
