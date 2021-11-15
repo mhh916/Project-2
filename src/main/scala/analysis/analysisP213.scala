22c787edb5c74aaf058c4ccd681d2c46be58fe68
Zach <brinner0@gmail.com>
2021-11-15 11:53:24 -0600

    UI guide text

--- src/main/scala/analysis/analysisP213.scala
+++ src/main/scala/analysis/analysisP213.scala
@@ -9,12 +9,16 @@ class analysisP213(spark:SparkConnect) {
   def q4(): Unit = {
     //move file to /user/maria_dev/Project2/Iowa_Liquor_Sales.csv
     //val df1 = spark.read.option("header",true).option("inferSchema",true).csv("/user/maria_dev/Project-2zb/Iowa_Liquor_Sales.csv")
+    println("Loading dataFrame...")
     val df1 = spark.getDataFrame()
-    val df2 = df1.withColumn("PPLiter (L/Per Dollar)", df1("Sale (Dollars)") / df1("Volume Sold (Liter)"))
-    val df3 = df2.groupBy("Category").agg(avg("PPLiter (P/Per Dollaar)").as("AVG_Price"))
+    val df2 = df1.withColumn("PPLiter (L/Per Dollar)", df1("Sale (Dollars)") / df1("Volume Sold (Liters)"))
+    val df3 = df2.groupBy("Category").agg(avg("PPLiter (L/Per Dollaar)").as("AVG_Price"))
+    println("Most Expensive Liquor Category")
     df3.orderBy(col("AVG_PPLiter").desc).show(5)
+    println("Least Expensive Liquor Category")
     df3.orderBy(col("AVG_PPLiter").asc).show(5)
     val df4 = df1.groupBy("Category").agg(sum("Volume Sold (Liters)")).as("SUM_Volume(L)")
+    println("Most Popular Liquor by Volume(Liters)")
     df4.orderBy(col("SUM_Value(L)").desc).show(5)
     }
   
