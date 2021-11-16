ead7cc59eae8d78c0cb4c4271a9140fd1d4147e0
Zach <brinner0@gmail.com>
2021-11-16 00:30:48 -0600

    Swapped Category Name for correct columns to query

--- src/main/scala/analysis/analysisP213.scala
+++ src/main/scala/analysis/analysisP213.scala
@@ -12,14 +12,14 @@ class analysisP213(spark:SparkConnect) {
     println("Loading dataFrame...")
     val df1 = spark.getDataFrame()
     val df2 = df1.withColumn("PPLiter (L/Per Dollar)", df1("Sale (Dollars)") / df1("Volume Sold (Liters)"))
-    val df3 = df2.groupBy("Category").agg(avg("PPLiter (L/Per Dollar)").as("AVG_Price"))
-    println("Most Expensive Liquor Category")
+    val df3 = df2.groupBy("Category Name").agg(avg("PPLiter (L/Per Dollar)").as("AVG_Price"))
+    println("Most Expensive Liquor (Category Name)")
     df3.orderBy(col("AVG_Price").desc).show(5)
     println("Least Expensive Liquor Category")
     df3.orderBy(col("AVG_Price").asc).show(5)
-    val df4 = df1.groupBy("Category").agg(sum("Volume Sold (Liters)")).as("SUM_Volume(L)")
+    val df4 = df1.groupBy("Category Name").agg(sum("Volume Sold (Liters)").as("SUM_Volume(L)"))
     println("Most Popular Liquor by Volume(Liters)")
-    df4.orderBy(col("SUM_Value(L)").desc).show(5)
+    df4.orderBy(col("SUM_Volume(L)").desc).show(5)
     }
   
 }
