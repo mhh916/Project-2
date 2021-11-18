1d56a7b77c151a3bbc481d80b9b135e7dc20be2d
Michael Hayes <michaelhayes22222@gmail.com>
2021-11-18 17:43:41 -0500

    Converted question 4 to run off parquet dataframe

--- src/main/scala/analysis/analysisP213.scala
+++ src/main/scala/analysis/analysisP213.scala
@@ -10,7 +10,7 @@ class analysisP213(spark:SparkConnect) {
     println("Loading dataFrame...")
     val df1 = spark.getDataFrame()
     //process RDD. Add columns, transform columns, and aggregate. Question 4 part 1&2
-    val df2 = df1.withColumn("PPLiter (L/Per Dollar)", df1("Sale (Dollars)") / df1("Volume Sold (Liters)")).withColumn("Category Name", lower(df1("Category Name"))).groupBy("Category Name").agg(avg("PPLiter (L/Per Dollar)").cast(DecimalType(10,2)).as("AVG_Price"))
+    val df2 = df1.withColumn("PPLiter (L/Per Dollar)", df1("Sale") / df1("VolumeSoldL")).withColumn("Category Name", lower(df1("CategoryName"))).groupBy("CategoryName").agg(avg("PPLiter (L/Per Dollar)").cast(DecimalType(10,2)).as("AVG_Price"))
     println("Most Expensive Liquor Category Name (Dollar/Liter)")
     //display top 5 results to Most Expensive Liquor
     df2.orderBy(col("AVG_Price").desc).show(5,false)
@@ -19,7 +19,7 @@ class analysisP213(spark:SparkConnect) {
     df2.orderBy(col("AVG_Price").asc).show(5,false)
     println("Most Popular Liquor by Volume(Barrels)")
     //process RDD. Add columns, transform columns, and aggregate. Question 4 part 3
-    val df3 = df1.withColumn("Category Name", lower(df1("Category Name"))).withColumn("Volume Sold (Barrels)",df1("Volume Sold (Gallons)")/53).groupBy("Category Name").agg(sum("Volume Sold (Barrels)").cast(DecimalType(10,2)).as("Total Volume (Barrels)")).orderBy(col("Total Volume (Barrels)").desc)
+    val df3 = df1.withColumn("Category Name", lower(df1("CategoryName"))).withColumn("Volume Sold (Barrels)",df1("VolumeSoldG")/53).groupBy("CategoryName").agg(sum("Volume Sold (Barrels)").cast(DecimalType(10,2)).as("Total Volume (Barrels)")).orderBy(col("Total Volume (Barrels)").desc)
     df3.show(5,false)
     }
 }
